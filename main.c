1a1b667aa12f09cb8764225a6d9c6264d4c3d102
Daniel Temiagin <talk2drys@gmail.com>
2023-05-23 02:29:15 -0600

    WIP: handling PATH and Environmental variable

--- main.c
+++ main.c
@@ -1,14 +1,9 @@
+#include "shell.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/wait.h>
 #include <unistd.h>
-
-#define MAX_COMMAND_LENGTH 100
-#define MAX_ARGUMENTS 10
-
-void handle_child_process(char *command, char **arguments);
-void handle_parent_process(pid_t pid, char *command);
 
 /**
  * main - Entry point of the simple_shell program
@@ -21,28 +16,24 @@ void handle_parent_process(pid_t pid, char *command);
  *
  * Return: Always 0.
  */
-int main(void)
+
+int main(int argc, char **argv, char **envp)
 {
 	char command[MAX_COMMAND_LENGTH];
 	char prompt[] = "#cisfun$ ";
 	pid_t pid;
-
 	while (1)
 	{
 		char *arguments[MAX_ARGUMENTS + 1];
 		char *token;
 		int arg_count = 0;
-
 		printf("%s", prompt);
-
 		if (fgets(command, sizeof(command), stdin) == NULL)
 		{
 			printf("\n");
 			break;
 		}
-
 		command[strcspn(command, "\n")] = '\0';
-
 		token = strtok(command, " ");
 		while (token != NULL && arg_count < MAX_ARGUMENTS)
 		{
@@ -50,34 +41,45 @@ int main(void)
 			token = strtok(NULL, " ");
 		}
 		arguments[arg_count] = NULL;
-
+		// Check if command exists in the PATH
+		char *command_path = get_command_path(arguments[0], envp);
+		if (command_path == NULL)
+		{
+			fprintf(stderr, "%s: command not found\n", arguments[0]);
+			continue;
+		}
 		pid = fork();
-
 		if (pid < 0)
 		{
 			perror("fork");
 			exit(1);
 		}
 		else if (pid == 0)
-			handle_child_process(arguments[0], arguments);
+		{
+			handle_child_process(command_path, arguments, envp);
+		}
 		else
+		{
 			handle_parent_process(pid, arguments[0]);
+		}
+		free(command_path);
 	}
-	return (0);
+	return 0;
 }
 
 /**
  * handle_child_process - Handles the child process after forking
  * @command: The command to be executed
  * @arguments: The arguments for the command
+ * #envp: pointer of char to environment variables
  *
  * This function is responsible for executing the specified command
  * with the given arguments in the child process using the `execve`
  * system call.
  */
-void handle_child_process(char *command, char **arguments)
+void handle_child_process(char *command, char **arguments, char **envp)
 {
-	int ret = execve(command, arguments, NULL);
+	int ret = execve(command, arguments, envp);
 
 	perror("execve");
 	exit(ret);
@@ -113,4 +115,3 @@ void handle_parent_process(pid_t pid, char *command)
 		}
 	}
 }
-
---
+++ shell.h
@@ -0,0 +1,13 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+#include <sys/types.h>
+
+#define MAX_COMMAND_LENGTH 100
+#define MAX_ARGUMENTS 10
+
+char *get_command_path(char *command, char **envp);
+void handle_child_process(char *command, char **arguments, char **envp);
+void handle_parent_process(pid_t pid, char *command);
+
+#endif
