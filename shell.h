cfdb0eb518fb1c1b0e149a44cb900ddfa15b2c45
Onome Temiagin <nomsy66@gmail.com>
2023-05-24 17:53:33 -0600

    update header file

--- shell.h
+++ shell.h
@@ -3,7 +3,7 @@
 
 #include <sys/types.h>
 
-#define MAX_COMMAND_LENGTH 100
+#define MAX_COMMAND_LENGTH 256
 #define MAX_ARGUMENTS 10
 
 char *get_command_path(char *command, char **envp);
@@ -12,5 +12,9 @@ void handle_parent_process(pid_t pid, char *command);
 void handle_command(char *command_path, char **arguments, pid_t *pid,
 										char **envp);
 void print_environment(char **envp);
+char *get_command_path(char *command, char **envp);
+
+char *get_absolute_command_path(char *command);
+char *search_command_in_path(char *command, char *path_env);
 
 #endif
